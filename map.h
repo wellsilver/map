42c76b3e765e03cb5de218dd014d1db265ac384b
wellsilver <wellsilver2016@gmail.com>
2022-11-23 16:20:10 -0500

    Update map.h

--- map.h
+++ map.h
@@ -5,20 +5,12 @@ typedef struct map2_graph {
     int y;
   }size;
   int x[MAP_SIZE][MAP_SIZE];
+#ifdef MAP_FUNC_PNTRS
+  int (*get)(int x,int y,struct map2_graph *map);
+  void (*set)(int x,int y,int set_to,struct map2_graph *map);
+  struct map2_find_ret (*find)(int to_find,struct map2_graph *map);
+#endif
 } map2_graph;
-map2_graph map2_new(int deflt) {
-  map2_graph i;
-  i.size.x = MAP_SIZE;
-  i.size.y = MAP_SIZE;
-  int loop;
-  int loop2;
-  for (loop=0;loop<MAP_SIZE;loop++) {
-    for (loop2=0;loop2<MAP_SIZE;loop2++) {
-      i.x[loop][loop2]=deflt;
-    }
-  }
-  return i;
-}
 int map2_get(int x,int y,map2_graph *map) {
   return map->x[x][y];
 }
@@ -46,6 +38,22 @@ struct map2_find_ret map2_find(int to_find,map2_graph *map) {
   e.y=-5;
   return e;
 }
+map2_graph map2_new(int deflt) {
+  map2_graph i;
+  i.get = map2_get;
+  i.set = map2_set;
+  i.find = map2_find;
+  i.size.x = MAP_SIZE;
+  i.size.y = MAP_SIZE;
+  int loop;
+  int loop2;
+  for (loop=0;loop<MAP_SIZE;loop++) {
+    for (loop2=0;loop2<MAP_SIZE;loop2++) {
+      i.x[loop][loop2]=deflt;
+    }
+  }
+  return i;
+}
 #else // dynamic map sizes (malloc)
 #include <stdlib.h>
 typedef struct map2_graph {
@@ -54,25 +62,12 @@ typedef struct map2_graph {
     int y;
   }size;
   int **x;
+#ifdef MAP_FUNC_PNTRS
+  int (*get)(int x,int y,struct map2_graph *map);
+  void (*set)(int x,int y,int set_to,struct map2_graph *map);
+  struct map2_find_ret (*find)(int to_find,struct map2_graph *map);
+#endif
 } map2_graph;
-// make a new graph thats x*y large and all keys are set to deflt
-map2_graph map2_new(int x,int y,int deflt) {
-  map2_graph i;
-  i.size.x = x;
-  i.size.y = y;
-  int loop;
-  i.x = (int **) malloc(sizeof(int **)*x);
-  for (loop=0;loop<x;loop++) {
-    i.x[loop] = (int *) malloc(sizeof(int *)*y);
-  }
-  int loop2;
-  for (loop=0;loop<x;loop++) {
-    for (loop2=0;loop2<y;loop2++) {
-      i.x[loop][loop2] = deflt;
-    }
-  }
-  return i;
-}
 // get and return x_y
 int map2_get(int x,int y,map2_graph *map) {
   return map->x[x][y];
@@ -102,5 +97,26 @@ struct map2_find_ret map2_find(int to_find,map2_graph *map) {
   e.x=-5;
   e.y=-5;
   return e;
+}
+// make a new graph thats x*y large and all keys are set to deflt
+map2_graph map2_new(int x,int y,int deflt) {
+  map2_graph i;
+  i.get = map2_get;
+  i.set = map2_set;
+  i.find = map2_find;
+  i.size.x = x;
+  i.size.y = y;
+  int loop;
+  i.x = (int **) malloc(sizeof(int **)*x);
+  for (loop=0;loop<x;loop++) {
+    i.x[loop] = (int *) malloc(sizeof(int *)*y);
+  }
+  int loop2;
+  for (loop=0;loop<x;loop++) {
+    for (loop2=0;loop2<y;loop2++) {
+      i.x[loop][loop2] = deflt;
+    }
+  }
+  return i;
 }
 #endif
